ba8d617a621ad34dff8309e8648e7fe7e014a961
self-unit <self_unit@hotmail.com>
2018-10-08 18:16:28 +0100

    Final homework solution

--- src/main/java/Printer.java
+++ src/main/java/Printer.java
@@ -1,2 +1,42 @@
 public class Printer {
+    private int sheetsOfPaper = 100;
+    private int tonerVolume = 50;
+
+    public Printer(){
+        this.sheetsOfPaper = sheetsOfPaper;
+        this.tonerVolume = tonerVolume;
+    }
+
+    public String print(int pages, int copies) {
+        int totalPages = (pages * copies);
+        if (this.sheetsOfPaper >= totalPages) {
+            if (this.tonerVolume >= totalPages) {
+                this.sheetsOfPaper -= totalPages;
+                this.tonerVolume -= totalPages;
+                return(String.format("Printed %d pages. There are %d sheets of paper and %d volumes of toner left in the cartridge.", totalPages, this.sheetsOfPaper, this.tonerVolume));
+            } else {
+                return(String.format("There are only %d volumes of toner and you require %d for your print request!", this.tonerVolume, totalPages));
+            }
+        } else {
+            return(String.format("There are only %d sheets of paper and you require %d for your print request!", this.sheetsOfPaper, totalPages));
+        }
+
+    }
+
+    public int refillPaper() {
+        return this.sheetsOfPaper = 100;
+    }
+
+    public int refillToner() {
+        return this.tonerVolume = 50;
+    }
+
+    public int getSheetsOfPaper() {
+        return this.sheetsOfPaper;
+    }
+
+    public int getTonerVolume() {
+        return this.tonerVolume;
+    }
+
 }
--- src/test/java/PrinterTest.java
+++ src/test/java/PrinterTest.java
@@ -1,2 +1,42 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PrinterTest {
+    Printer printer;
+    @Before
+    public void before() {
+        printer = new Printer();
+    }
+
+    @Test
+    public void hasPaper() {
+        assertEquals(100, printer.getSheetsOfPaper());
+    }
+    @Test
+    public void hasToner() {
+        assertEquals(50, printer.getTonerVolume());
+    }
+    @Test
+    public void canPrint() {
+        assertEquals("Printed 40 pages. There are 60 sheets of paper and 10 volumes of toner left in the cartridge.", printer.print(10, 4));
+    }
+    @Test
+    public void lowOnToner() {
+        assertEquals("There are only 50 volumes of toner and you require 60 for your print request!", printer.print(10, 6));
+    }
+    @Test
+    public void lowOnPaper() {
+        assertEquals("There are only 100 sheets of paper and you require 120 for your print request!", printer.print(10, 12));
+    }
+    @Test
+    public void canRefillPaper() {
+        assertEquals(100, printer.getSheetsOfPaper());
+    }
+    @Test
+    public void canRefillToner() {
+        assertEquals(50, printer.getTonerVolume());
+    }
+
 }
